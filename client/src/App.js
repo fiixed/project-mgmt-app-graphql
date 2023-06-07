fd59dbc7e1229f9d496588651d000a132ea5b42e
Andrew Bell <fiixed@gmail.com>
2023-06-07 11:11:04 -0500

    add clients frontend

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,30 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import Clients from './components/Clients';
+import AddClientModal from './components/AddClientModal';
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
 
 const client = new ApolloClient({
   uri: 'http://localhost:8000/graphql',
-  cache: new InMemoryCache(),
+  cache,
 });
 
 function App() {
@@ -15,6 +35,7 @@ function App() {
       <Router>
         <Header />
         <div className="container">
+          <AddClientModal />
           <Clients />
           <Routes>
             {/* <Route path="/" element={<Home />} />
