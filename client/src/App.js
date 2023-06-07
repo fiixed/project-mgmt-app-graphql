d24f50eea213e308d6bbfb2fe7b43637cc1f61e9
Andrew Bell <fiixed@gmail.com>
2023-06-07 11:30:30 -0500

    react routing

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import Clients from './components/Clients';
 import AddClientModal from './components/AddClientModal';
 import Projects from './components/Projects';
+import Home from './pages/Home';
+import Project from './pages/Project';
+import NotFound from './pages/NotFound';
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -36,13 +39,10 @@ function App() {
         <Router>
           <Header />
           <div className="container">
-            <AddClientModal />
-            <Projects />
-            <Clients />
             <Routes>
-              {/* <Route path="/" element={<Home />} />
+              <Route path="/" element={<Home />} />
               <Route path="/projects/:id" element={<Project />} />
-              <Route path="*" element={<NotFound />} /> */}
+              <Route path="*" element={<NotFound />} />
             </Routes>
           </div>
         </Router>
