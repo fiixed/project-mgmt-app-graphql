92d28076c40f6242eaebbf3332365c8147e9e7d2
Andrew Bell <fiixed@gmail.com>
2023-07-10 16:13:14 -0500

    update prod uri

--- client/src/App.js
+++ client/src/App.js
@@ -28,7 +28,7 @@ const cache = new InMemoryCache({
 });
 
 const client = new ApolloClient({
-  uri: 'https://project-mgmt-app-graphql-backend.onrender.com:8000/graphql',
+  uri: 'https://project-mgmt-app-graphql-backend.onrender.com/graphql',
   cache,
 });
 
